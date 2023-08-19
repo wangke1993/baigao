f5b279fe147c020fd6915c51b2acc4e1bf6059c5
v_vkewang <v_vkewang@tencent.com>
2023-08-19 22:13:46 +0800

    新增字典管理及系统配置TODO项

--- backend/src/system-config/system-config.module.ts
+++ backend/src/system-config/system-config.module.ts
@@ -5,6 +5,17 @@ import { SystemConfigController } from './system-config.controller';
 import { SystemConfigService } from './system-config.service';
 import { SystemConfig, SystemConfigSchema } from './dto/system-config.schema';
 
+/**
+ * TODO 实现自定义配置，重构SystemConfig
+ * 分组
+ *  UUID
+ *  分组名称
+ *  标签名称
+ *  标签链接
+ * 分组元素关系
+ *  分组UUID
+ *  dicCode：不能重复
+ */
 @Module({
     imports: [MongooseModule.forFeature([{ name: SystemConfig.name, schema: SystemConfigSchema }]), SystemLogModule],
     providers: [SystemConfigService],
