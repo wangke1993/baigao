66049ffba233275ffdc40dfd974f252d0cc0eb2b
v_vkewang <v_vkewang@tencent.com>
2024-04-23 12:01:57 +0800

    新增pm2启动配置文件

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'BG-server',
+      //TODO:修改为实际的地址
+      script: 'main.js',
+      // 副本数量（推荐于cpu核心数相同）
+      instances: 4,
+      exec_mode: 'cluster',
+      env: {},
+    },
+  ],
+};
